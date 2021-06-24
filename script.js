2778b357f4c9980f743dcf555b17b95b2188f8fa
Saiban Hussein <shhussein@m.marywood.edu>
2021-06-24 16:38:05 -0500

    Add all files

---
+++ script.js
@@ -0,0 +1,67 @@
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      title: "Arrays in Javascript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+      answer: "quotes"
+  },
+  {
+      title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+      choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+      answer: "console log"
+  },
+
+];
+
+// Selects element by class
+var ulEl=document.querySelector("choicesUl");
+// Selects element by id
+
+var mainEl = document.getElementById("main");
+
+var secondsLeft=75;
+var secondsLeft = 10;
+
+
+
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
